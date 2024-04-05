c81895db6be34d3783dfdd6281a47627832a2027
kumarumesh01 <160713149+kumarumesh01@users.noreply.github.com>
2024-04-06 01:53:09 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'your_database_user',
-  host: 'your_database_host',
-  database: 'your_database_name',
-  password: 'your_database_password',
-  port: 5432,
+  user: 'learner', // Your database user
+  host: 'neetprep-staging.cvvtorjqg7t7.ap-south-1.rds.amazonaws.com', // Your database host
+  database: 'learner_development', // Your database name
+  password: 'Deq05h0KiL6icSvS', // Your database password
+  port: 5432, // Default PostgreSQL port
 });
 
 module.exports = pool;
